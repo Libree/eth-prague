b0504f6cb546b57ad33d1c6c4ab2b25eaf4b0601
Ivan <ivan.alberquilla@gmail.com>
2023-06-09 16:58:19 +0200

    module functions

---
+++ lens-protocol/contracts/core/modules/TokenizedSubFollowModule.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import {Errors} from '../../../libraries/Errors.sol';
+import {ModuleBase} from '../ModuleBase.sol';
+import {IFollowModule} from '../../../interfaces/IFollowModule.sol';
+import {FollowValidatorFollowModuleBase} from './FollowValidatorFollowModuleBase.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+
+struct ProfileData {
+    address currency;
+    uint256 amount;
+    address recipient;
+}
+
+contract TokenizedSubFollowModule is IFollowModule, FollowValidatorFollowModuleBase {
+    using SafeERC20 for IERC20;
+
+    mapping(uint256 => ProfileData) internal _dataByProfile;
+
+    constructor(address hub) ModuleBase(hub) {}
+
+    function initializeFollowModule(
+        uint256 profileId,
+        bytes calldata data
+    ) external override onlyHub returns (bytes memory) {
+        _dataByProfile[profileId].amount = amount;
+        _dataByProfile[profileId].currency = currency;
+        _dataByProfile[profileId].recipient = address(tokenizedSub);
+    }
+
+    function processFollow(
+        address follower,
+        uint256 profileId,
+        bytes calldata data
+    ) external override onlyHub {
+        uint256 amount = _dataByProfile[profileId].amount;
+        address currency = _dataByProfile[profileId].currency;
+        address recipient = _dataByProfile[profileId].recipient;
+    }
+
+    function followModuleTransferHook(
+        uint256 profileId,
+        address from,
+        address to,
+        uint256 followNFTTokenId
+    ) external override {}
+
+    function getProfileData(uint256 profileId) external view returns (ProfileData memory) {
+        return _dataByProfile[profileId];
+    }
+}
